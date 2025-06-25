64b01622d7e414cd96d1cfb80e8668ceb2df941c
jaydamani <jaydamani567@gmail.com>
2025-06-25 06:21:26 +0530

    refactor: add builtins array feat: add type builtin

--- src/main.c
+++ src/main.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUILTINS_COUNT 3
+
+char *cmd_list[BUILTINS_COUNT] = {"exit", "echo", "type"};
+
+int find_builtin_cmd(char *cmd) {
+  for (int i = 0; i < BUILTINS_COUNT; i++) {
+    if (strcmp(cmd_list[i], cmd) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   char input[100];
@@ -18,14 +31,26 @@ int main(int argc, char *argv[]) {
     tokenize(input, &t);
 
     char *cmd = t->value;
-    if (strncmp(cmd, "exit", 4) == 0) {
+
+    int builtin = find_builtin_cmd(cmd);
+    if (builtin == 0) {
       struct token *arg0 = t->next;
       if (arg0 != NULL) {
         exitcode = atoi(t->next->value);
       }
       break;
-    } else if (strncmp(cmd, "echo", 4) == 0) {
+    } else if (builtin == 1) {
       printToken(t->next);
+    } else if (builtin == 2) {
+      struct token *arg0_t = t->next;
+      if (arg0_t != NULL) {
+        char *arg0 = arg0_t->value;
+        if (find_builtin_cmd(arg0) != -1) {
+          printf("%s is a shell builtin\n", arg0);
+        } else {
+          printf("%s: not found\n", arg0);
+        }
+      }
     } else {
       printf("%s: command not found\n", cmd);
     }
